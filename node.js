1e9f7f7c43a666a49c684ffe6de294c7ae581481
zuhito <zuhito@hotmail.co.jp>
2018-08-12 22:23:26 +0900

    Support typedInput

--- node.js
+++ node.js
@@ -1,13 +1,23 @@
 var geolib = require('geolib');
 var radar = require('flightradar24-client/lib/radar');
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function Flightradar24Node(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function (msg) {
-            var lat = Number(config.lat);
-            var lon = Number(config.lon);
+            var lat, lon;
+            if (config.latType === 'num') {
+                lat = Number(config.lat);
+            } else {
+                lat = Number(RED.util.getMessageProperty(msg, config.lat));
+            }
+            if (config.lonType === 'num') {
+                lon = Number(config.lon);
+            } else {
+                lon = Number(RED.util.getMessageProperty(msg, config.lon));
+            }
+
             var radius = 100000;
             radar(lat + 1, lon - 1, lat - 1, lon + 1).then(function (data) {
                 data.forEach(function (flight) {
@@ -30,5 +40,5 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("flightradar24", Flightradar24Node);
-}
+};
 
